726b12c2b05976ea943ce911862c2a4704e99301
Pavel Yasonau <megomozg@gmail.com>
2017-10-13 00:51:46 +0300

    report improvments

--- LykkeAutomation/run_tests_via_console.bat
+++ LykkeAutomation/run_tests_via_console.bat
@@ -1,3 +1,4 @@
 path=%cd%
 %path%\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe %path%\bin\Debug\LykkeAutomation.dll --where "cat==PersonalData"
 c:\LykkeAutomation_Report\index.html
+pause
