8e841b6d1218e5cca2ffe266dc0c3e0a5628bcfd
Pavel Yasonau <megomozg@gmail.com>
2017-10-23 00:35:41 +0300

    MSTestAllureAdapter.Console.exe(Framework 4.0) -> MSTestAllureAdapter.Console.dll(.net core 2.0)

--- LykkeAutomation/TestReportHelpers/create_report.cmd
+++ LykkeAutomation/TestReportHelpers/create_report.cmd
@@ -1,5 +1,5 @@
 set currentDir=%cd%
 dotnet test ../LykkeAutomation.csproj --filter "TestCategory=PersonalData" --logger "trx;LogFileName=%currentDir%/report.trx"
-MSTestAllureAdapter.Console.exe report.trx output-xmls
+dotnet MSTestAllureAdapter.Console.dll report.trx output-xmls
 %currentDir%/allure-cli/bin/allure generate %currentDir% -v 1.4.0
 pause
