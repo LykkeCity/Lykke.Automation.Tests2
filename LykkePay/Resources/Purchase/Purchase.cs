47f1229ecf8858f1a4e627b0bf5d224cf071aef0
Pavel Yasonau <megomozg@gmail.com>
2017-11-14 00:16:36 +0300

    Purchase resource api

---
+++ LykkePay/Resources/Purchase/Purchase.cs
@@ -0,0 +1,30 @@
+﻿using LykkePay.Models;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestsCore.ApiRestClient;
+
+namespace LykkePay.Resources.Purchase
+{
+    public class Purchase : RestApi
+    {
+        private const string resource = "/purchase";
+
+        public IRestResponse PostPurchaseResponse(PostMerchantModel merchantModel, PostPurchaseModel purchaseModel)
+        {
+            var request = new RestRequest(resource, Method.POST);
+            request.AddHeader("Lykke-Merchant-Id", merchantModel.LykkeMerchantId);
+            request.AddHeader("Lykke-Merchant-Sign", merchantModel.LykkeMerchantSign);
+            request.AddHeader("Lykke-Merchant-Session-Id", merchantModel.LykkeMerchantSessionID);
+            request.AddJsonBody(purchaseModel);
+            var response = client.Execute(request);
+
+            return response;
+        }
+
+        public PostPurchaseResponseModel PostPurchaseResponseModel(PostMerchantModel merchantModel, PostPurchaseModel purchaseModel) =>
+            JsonConvert.DeserializeObject<PostPurchaseResponseModel>(PostPurchaseResponse(merchantModel, purchaseModel).Content);
+    }
+}
