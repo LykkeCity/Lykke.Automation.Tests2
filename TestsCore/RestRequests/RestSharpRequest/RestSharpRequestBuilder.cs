283e025517d407d1c286017c9d4ba70231e18714
Andrey Gurenkov <undron@users.noreply.github.com>
2017-11-19 17:42:22 +0300

    Logging added

--- TestsCore/RestRequests/RestSharpRequest/RestSharpRequestBuilder.cs
+++ TestsCore/RestRequests/RestSharpRequest/RestSharpRequestBuilder.cs
@@ -5,6 +5,9 @@ using System.Net;
 using System.Text;
 using TestsCore.RestRequests.Interfaces;
 using Newtonsoft.Json;
+using System.Linq;
+using LykkeAutomation.TestsCore;
+using NUnit.Framework;
 
 namespace TestsCore.RestRequests.RestSharpRequest
 {
@@ -39,12 +42,15 @@ namespace TestsCore.RestRequests.RestSharpRequest
         public IResponse Execute()
         {
             var response = client.Execute(request);
+            Log(response);
             return new Response { StatusCode = response.StatusCode, Content = response.Content };
         }
 
         public T Execute<T>() where T : new()
         {
-            return client.Execute<T>(request).Data;
+            var response = client.Execute<T>(request);
+            Log(response);
+            return response.Data;
         }
 
         public IRequestBuilder WithProxy
@@ -93,6 +99,27 @@ namespace TestsCore.RestRequests.RestSharpRequest
         {
             request.AddHeader("Accept", mediaType);
             return this;
+        }
+        #endregion
+        #region Private methods
+        private void Log(IRestResponse response)
+        {
+            var requestBody = response.Request.Parameters.FirstOrDefault(p => p.Type == ParameterType.RequestBody);
+
+            string attachName = $"{response.Request.Method} {response.Request.Resource}";
+            var attachContext = new StringBuilder();
+            attachContext.AppendLine($"Executing {response.Request.Method} {response.ResponseUri}");
+            if (requestBody != null)
+            {
+                attachContext.AppendLine($"Content-Type: {requestBody.ContentType}").AppendLine();
+                attachContext.AppendLine(requestBody.Value.ToString());
+            }
+            attachContext.AppendLine().AppendLine();
+            attachContext.AppendLine($"Response: {response.StatusCode}");
+            attachContext.AppendLine(response.Content);
+
+            AllureReport.GetInstance().AddAttachment(TestContext.CurrentContext.Test.FullName,
+                Encoding.UTF8.GetBytes(attachContext.ToString()), attachName, "application/json");
         }
         #endregion
     }
