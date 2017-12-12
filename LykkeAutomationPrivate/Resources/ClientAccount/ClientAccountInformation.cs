435c93e97df4fcd7af7f524df6bd401d75522782
Andrey Hurankou <andrey.hurankou@lykke.com>
2017-12-12 22:57:17 +0300

    Client Account Service: ClientAccountInformation few tests

--- LykkeAutomationPrivate/Resources/ClientAccount/ClientAccountInformation.cs
+++ LykkeAutomationPrivate/Resources/ClientAccount/ClientAccountInformation.cs
@@ -13,5 +13,24 @@ namespace LykkeAutomationPrivate.Resources.ClientAccountResource
             return Request.Get("/api/ClientAccountInformation").AddQueryParameter("id", id)
                 .Build().Execute<ClientAccountInformation>();
         }
+
+        //TODO: Body in GET does not alowed https://msdn.microsoft.com/en-us/library/d4cek6cc%28v=vs.110%29.aspx
+        //public IResponse<List<ClientAccountInformation>> GetClientsByIds(ClientAccountIdsModel ids)
+        //{
+        //    return Request.Get("/api/ClientAccountInformation/getClientsByIds").AddJsonBody(ids)
+        //        .Build().Execute<List<ClientAccountInformation>>();
+        //}
+
+        public IResponse<List<string>> GetClientsByPhone(string phoneNumber)
+        {
+            return Request.Get($"/api/ClientAccountInformation/getClientsByPhone/{phoneNumber}")
+                .Build().Execute<List<string>>();
+        }
+
+        public IResponse<bool> GetIsPasswordCorrect(string clientId, string password)
+        {
+            return Request.Get($"/api/ClientAccountInformation/isPasswordCorrect/{clientId}/{password}")
+                .Build().Execute<bool>();
+        }
     }
 }
--- LykkeAutomationPrivate/Tests/ClientAccount/ClientAccountInformationTests.cs
+++ LykkeAutomationPrivate/Tests/ClientAccount/ClientAccountInformationTests.cs
@@ -1,4 +1,5 @@
 ﻿using LykkeAutomationPrivate.DataGenerators;
+using LykkeAutomationPrivate.Models.ClientAccount.Models;
 using LykkeAutomationPrivate.Models.Registration.Models;
 using NUnit.Framework;
 using System;
@@ -10,15 +11,15 @@ namespace LykkeAutomationPrivate.Tests.ClientAccount
 {
     class GetClientAccountInformation : BaseTest
     {
-        AccountRegistrationModel registrationModel;
+        AccountRegistrationModel registration;
         ClientAccountInformationModel account;
         string pin = "1111";
 
-        [SetUp]
+        [OneTimeSetUp]
         public void CreateClient()
         {
-            registrationModel = new AccountRegistrationModel().GetTestModel();
-            account = lykkeApi.Registration.PostRegistration(registrationModel).Account;
+            registration = new AccountRegistrationModel().GetTestModel();
+            account = lykkeApi.Registration.PostRegistration(registration).Account;
             lykkeApi.ClientAccount.Wallets.PostClientAccountInformationsetPIN(account.Id, pin);
         }
 
@@ -31,15 +32,57 @@ namespace LykkeAutomationPrivate.Tests.ClientAccount
             Assert.That(getAccountInformation.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
             var accountInformation = getAccountInformation.GetResponseObject();
-            Assert.That(accountInformation.Email, Is.EqualTo(registrationModel.Email));
+            Assert.That(accountInformation.Email, Is.EqualTo(registration.Email));
             Assert.That(accountInformation.Id, Is.EqualTo(account.Id));
             Assert.That(accountInformation.IsReviewAccount, Is.EqualTo(false));
             Assert.That(accountInformation.IsTrusted, Is.EqualTo(false));
             Assert.That(accountInformation.NotificationsId, Is.EqualTo(account.NotificationsId));
             Assert.That(accountInformation.PartnerId, Is.Null);
-            Assert.That(accountInformation.Phone, Is.EqualTo(registrationModel.ContactPhone));
+            Assert.That(accountInformation.Phone, Is.EqualTo(registration.ContactPhone));
             Assert.That(accountInformation.Pin, Is.EqualTo(pin));
             Assert.That(accountInformation.Registered, Is.EqualTo(DateTime.UtcNow).Within(TimeSpan.FromMinutes(10)));
+        }
+
+        //TODO: Body in GET does not alowed https://msdn.microsoft.com/en-us/library/d4cek6cc%28v=vs.110%29.aspx
+        //[Test]
+        //[Description("Get clients by ids.")]
+        //[Category("ClientAccountInformationResource"), Category("ClientAccount"), Category("ServiceAll")]
+        //public void GetClientsByIdsTest()
+        //{
+        //    var clientAccountIds = new ClientAccountIdsModel()
+        //    {
+        //        Ids = new List<string>() { account.Id }
+        //    };
+
+        //    var getClientsByIds = lykkeApi.ClientAccount.ClientAccountInformation
+        //        .GetClientsByIds(clientAccountIds);
+        //    Assert.That(getClientsByIds.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+        //    var clientsByIds = getClientsByIds.GetResponseObject();
+        //}
+
+        [Test]
+        [Description("Get clients by phone.")]
+        [Category("ClientAccountInformationResource"), Category("ClientAccount"), Category("ServiceAll")]
+        public void GetClientsByPhoneTest()
+        {
+            var getClientsByPhone = lykkeApi.ClientAccount.ClientAccountInformation
+                .GetClientsByPhone(registration.ContactPhone);
+            Assert.That(getClientsByPhone.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            Assert.That(getClientsByPhone.GetResponseObject(), Does.Contain(account.Id));
+        }
+
+        [Test]
+        [Description("Check if password is correct.")]
+        [Category("ClientAccountInformationResource"), Category("ClientAccount"), Category("ServiceAll")]
+        public void GetIsPasswordCorrectTest()
+        {
+            var isPasswordCorrect = lykkeApi.ClientAccount.ClientAccountInformation
+                .GetIsPasswordCorrect(account.Id, registration.Password);
+            Assert.That(isPasswordCorrect.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            Assert.That(isPasswordCorrect.GetResponseObject(), Is.True);
         }
     }
 }
